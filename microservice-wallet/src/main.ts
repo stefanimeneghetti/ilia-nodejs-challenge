8a3715df54b21ce821049b70cef9f8d0bf7db2f0
Stefani Meneghetti <meneghettistefani@gmail.com>
2026-02-11 19:58:16 -0300

    feat: initial nest, prisma and docker configuration

---
+++ microservice-wallet/src/main.ts
@@ -0,0 +1,24 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { config } from 'dotenv';
+
+async function bootstrap() {
+  config();
+  const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      disableErrorMessages: false,
+    }),
+  );
+
+  const port = process.env.PORT ?? 3001;
+
+  await app.listen(port);
+}
+
+bootstrap();
